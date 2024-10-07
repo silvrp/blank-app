ebc8309a5a45a9b2c3bfbca544ea51cf073aaa14
silvrp <srianop@caixabank.com>
2024-10-07 09:03:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Hello World")
+st.text(
+    "Testing"
 )
